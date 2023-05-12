acc90a504d9388b1d6002ef7406e42619d7e058b
Archeveque <114493055+Archeveque@users.noreply.github.com>
2023-05-12 09:17:01 +0200

    updated some files

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,15 @@ nice = City.create(name: " Nice")
 7.times do
   City.create(name: Faker::Games::SuperSmashBros.stage)
 end
-  user = User.create!(pseudo: "R0g3r",description: Faker::Lorem.sentence(word_count: 15, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris)
+  user = User.create!(pseudo: "R0g3r",description: Faker::Lorem.sentence(word_count: 15, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris,password: Faker::Name.first_name)
 
   9.times do
-    user = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence(word_count: 15, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris)
+    user = User.create!(pseudo: Faker::Name.first_name,description: Faker::Lorem.sentence(word_count: 15, random_words_to_add: 6),email: Faker::Internet.email,age: rand(8..80),city: paris,password: Faker::Name.first_name)
   end
 puts " users done"
   20.times do
     gossip = Gossip.create!(title: Faker::Name.first_name,content: Faker::Lorem.sentence(word_count: 25, random_words_to_add: 10),user: User.all.sample)
+  end
+  40.times do
+    comment = Comment.create(user: User.all.sample,gossip: Gossip.all.sample,content:Faker::Lorem.sentence(word_count: 10, random_words_to_add: 10))
   end
